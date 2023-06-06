28846cd49ba1cec33375feddc1b0411d685e7aa3
José Augusto <jose.jesus@gbmx.com.br>
2023-06-06 13:42:05 -0300

    comment: added to method fill in Model.php

--- src/database/Model.php
+++ src/database/Model.php
@@ -4,7 +4,14 @@ namespace src\database;
 
 class Model
 {
-    public function fill(array $data)
+    /**
+     * Method responsible for filling the model attributes according to the informed array
+     * 
+     * @param array $data - indexed content that will be filled as attributes of the model
+     * 
+     * @return self
+     */
+    public function fill(array $data): self
     {
         foreach ($data as $name => $value) {
             $this->$name = $value;

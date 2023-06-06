55c5a0179a0772973758ca88c8d69d6000b44101
José Augusto <jose.jesus@gbmx.com.br>
2023-06-06 16:56:48 -0300

    feat: implements constants for messages

---
+++ src/helpers/messages.php
@@ -0,0 +1,5 @@
+<?php
+
+define("FALHA_AO_SALVAR", "Whoops, aconteceu um problema ao tentar salvar os dados. Por favor, tente novamente.");
+
+define("SALVO_COM_SUCESSO", "Os informados foram salvos com sucesso");
